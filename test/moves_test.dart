47c15a47725e20ab749bc0396f31092191e10f0f
synw <ansterj@gmail.com>
2019-11-06 04:02:37 +0100

    Update tests

--- test/moves_test.dart
+++ test/moves_test.dart
@@ -7,9 +7,9 @@ void main() {
       geometryCol: "geopoint", speedCol: "speed", timestampCol: "date");
 
   test("moves", () async {
-    expect(df.numRows, 100);
+    expect(df.length, 100);
     //print("DATA ${df.data}");
-    print("Zero speed: ${df.countZeros("speed")}");
+    print("Zero speed: ${df.countZeros_("speed")}");
     final tls = await df.moves(minStopDuration: const Duration(seconds: 1));
     print(tls);
     expect(tls.numPoints > 0, true);
