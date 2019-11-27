e880b4297cd2a5a8bc44e7bc8fc52c27755d7dad
synw <synw@yahoo.com>
2019-11-27 19:49:31 +0100

    Update example

--- example/main.dart
+++ example/main.dart
@@ -1,8 +1,7 @@
 import 'package:geodf/geodf.dart';
 
 Future<void> main(List<String> args) async {
-  await GeoDataFrame.fromGeoJsonFile("data/positions_sample.geojson",
+  final df = await GeoDataFrame.fromGeoJsonFile("data/positions_sample.geojson",
       timestampFormat: TimestampType.seconds, verbose: true)
-    ..cols()
-    ..headCol("speed");
+    ..show();
 }
